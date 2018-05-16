44d7b1315589df7444db6e4c027938f2336f9a0d
David Wilhelm <dave@jumbledpile.com>
2018-05-15 21:43:28 -0700

    Add a build script for Qt 5.10 and VS 2017
    For now, the build script has hard coded paths for my setup:
    - C:\Qt\Qt5.10.0\5.10.0\msvc2017_64
    - C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat

---
+++ kdiff3/buildTools/build_kdiff3.bat
@@ -0,0 +1,34 @@
+@echo off
+
+set SRCROOT=%~d0%~p0..\..
+set DEBUG_DIR=Debug
+set RELEASE_DIR=Release
+
+cd %SRCROOT%
+
+REM Set up Qt environment
+set QTDIR=C:\Qt\Qt5.10.0\5.10.0\msvc2017_64
+set PATH=%QTDIR%\bin;%PATH%
+
+REM Set up Visual Studio environment variables.
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+cd /d %SRCROOT%\kdiff3
+qmake .\src-QT4\kdiff3.pro
+nmake clean
+
+if x%1==xdebug (
+    if not exist %DEBUG_DIR% mkdir %DEBUG_DIR%
+    copy %QTDIR%\bin\Qt5Cored.dll %DEBUG_DIR%
+    copy %QTDIR%\bin\Qt5Guid.dll %DEBUG_DIR%
+    copy %QTDIR%\bin\Qt5PrintSupportd.dll %DEBUG_DIR%
+    copy %QTDIR%\bin\Qt5Widgetsd.dll %DEBUG_DIR%
+    nmake debug
+) else if not x%1==xclean (
+    if not exist %RELEASE_DIR% mkdir %RELEASE_DIR%
+    copy %QTDIR%\bin\Qt5Core.dll %RELEASE_DIR%
+    copy %QTDIR%\bin\Qt5Gui.dll %RELEASE_DIR%
+    copy %QTDIR%\bin\Qt5PrintSupport.dll %RELEASE_DIR%
+    copy %QTDIR%\bin\Qt5Widgets.dll %RELEASE_DIR%
+    nmake release
+    nmake clean
+)
