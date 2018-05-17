174e0dbeb4b5b297bce8963b2b9f7ad754e832d1
David Wilhelm <dave@jumbledpile.com>
2018-05-17 01:52:31 -0700

    Add VERSIONINFO data to the Windows resource file
    For syntax, language codes, and character set codes, see
    https://msdn.microsoft.com/en-us/library/windows/desktop/aa381058(v=vs.85).aspx
    
    This is stolen from
    https://stackoverflow.com/questions/2784697/setting-application-info-in-a-qt-executable-file-on-windows

--- kdiff3/src-QT4/version.h
+++ kdiff3/src-QT4/version.h
@@ -3,4 +3,11 @@
 
 #define VERSION "0.9.98"
 
+#define VER_FILEVERSION             0,9,98,0
+#define VER_FILEVERSION_STR         "0.9.98.0\0"
+#define VER_ORIGINALFILENAME_STR    "kdiff3.exe"
+#define VER_PRODUCTNAME_STR         "KDiff3"
+#define VER_PRODUCTVERSION          0,9,98,0
+#define VER_PRODUCTVERSION_STR      "0.9.98\0"
+
 #endif // VERSION_H
