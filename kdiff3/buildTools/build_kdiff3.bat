6b31638cc4af5e990deb28f89fe3872e1dd34252
David Wilhelm <dave@jumbledpile.com>
2018-05-28 01:59:35 -0700

    Consolidate setgitversion calls in build script
    There's no harm in setting the version temporarily, even when
    cleaning, so keep the script as simple as possible.

--- kdiff3/buildTools/build_kdiff3.bat
+++ kdiff3/buildTools/build_kdiff3.bat
@@ -14,9 +14,10 @@ cd /d %SRCROOT%\kdiff3
 qmake .\src-QT4\kdiff3.pro
 nmake clean
 
+powershell -ExecutionPolicy Bypass -File .\buildTools\setgitversion.ps1
+
 if x%1==xdebug (
     if not exist %DEBUG_DIR% mkdir %DEBUG_DIR%
-    powershell -ExecutionPolicy ByPass -File .\buildTools\setgitversion.ps1
     copy %QTDIR%\bin\Qt5Cored.dll %DEBUG_DIR%
     copy %QTDIR%\bin\Qt5Guid.dll %DEBUG_DIR%
     copy %QTDIR%\bin\Qt5PrintSupportd.dll %DEBUG_DIR%
@@ -24,7 +25,6 @@ if x%1==xdebug (
     nmake debug
 ) else if not x%1==xclean (
     if not exist %RELEASE_DIR% mkdir %RELEASE_DIR%
-    powershell -ExecutionPolicy ByPass -File .\buildTools\setgitversion.ps1
     copy %QTDIR%\bin\Qt5Core.dll %RELEASE_DIR%
     copy %QTDIR%\bin\Qt5Gui.dll %RELEASE_DIR%
     copy %QTDIR%\bin\Qt5PrintSupport.dll %RELEASE_DIR%
